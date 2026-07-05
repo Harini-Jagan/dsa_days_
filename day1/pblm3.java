ea7ea01bb2e3bdc43688b99d1703b3dc8146f179
Harini-Jagan <harini2410@gmail.com>
2026-07-05 12:30:11 +0530

    dsa

---
+++ day1/pblm3.java
@@ -0,0 +1,22 @@
+package day1;
+
+public class pblm3 {
+    public static void main(String[] args) {
+       int[] arr={17,98,99,53};
+       int largest=arr[0];
+       int secondlargest=arr[0];
+       for(int i=1;i<arr.length;i++){
+        if(arr[i]>largest){
+            secondlargest=largest;
+            largest=arr[i];
+        }
+        else if(arr[i]>secondlargest && arr[i]!=largest){
+            secondlargest=arr[i];
+        }
+        System.out.println("i = "+i+" ,largest="+largest+" ,secondlargest="+secondlargest);
+       } 
+       System.out.println("Secondlargest num is : "+secondlargest);
+       System.out.println("largest num is : "+largest);
+    }
+    
+}
